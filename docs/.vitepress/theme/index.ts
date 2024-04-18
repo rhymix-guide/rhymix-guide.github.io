f0a72d0e1cf451aae14c2e03f39df049589e158a
kkigomi <kkigomi@duck.com>
2024-04-18 23:55:21 +0900

    vitepress 업데이트 및 giscus 댓글 추가

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,10 +1,11 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
+import { h, toRefs } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-import './style.css'
-
+import giscusTalk from 'vitepress-plugin-comment-with-giscus';
 import googleAnalytics from 'vitepress-plugin-google-analytics'
+import { useData, useRoute } from 'vitepress';
+import './style.css'
 
 export default {
   extends: DefaultTheme,
@@ -17,5 +18,32 @@ export default {
     googleAnalytics({
       id: 'G-Q76QLPPKZY',
     })
+  },
+  setup() {
+    // Get frontmatter and route
+    const { frontmatter } = toRefs(useData());
+    const route = useRoute();
+
+    // Obtain configuration from: https://giscus.app/
+    giscusTalk({
+      repo: 'rhymix-guide/rhymix-guide.github.io',
+      repoId: 'R_kgDOK7dgrw',
+      category: 'Comments', // default: `General`
+      categoryId: 'DIC_kwDOK7dgr84CexuD',
+      mapping: 'pathname', // default: `pathname`
+      inputPosition: 'top', // default: `top`
+      lang: 'ko', // default: `zh-CN`
+      lightTheme: 'light', // default: `light`
+      darkTheme: 'transparent_dark', // default: `transparent_dark`
+      // ...
+    }, {
+      frontmatter, route
+    },
+      // Whether to activate the comment area on all pages.
+      // The default is true, which means enabled, this parameter can be ignored;
+      // If it is false, it means it is not enabled.
+      // You can use `comment: true` preface to enable it separately on the page.
+      true
+    );
   }
 } satisfies Theme
